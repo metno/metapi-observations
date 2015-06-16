e8964e5f7866f7a5b51f2f8ee6d64767dbc00d7f
Jo Asplin <jo.asplin@met.no>
2015-06-16 14:30:30 +0200

    Added more time spec tests
    Summary: Added more time spec tests, covering more of ISO 8601.
    
    Test Plan: activator test
    
    Reviewers: #met_api, vegardb
    
    Reviewed By: #met_api, vegardb
    
    Subscribers: vegardb
    
    Maniphest Tasks: T748
    
    Differential Revision: https://phab.met.no/D708

--- test/TimeSpecificationSpec.scala
+++ test/TimeSpecificationSpec.scala
@@ -38,28 +38,10 @@ import scala.util._
 class TimeSpecificationSpec extends Specification {
 
   "TimeSpecification" should {
-    "parse single time range" in {
-      val start = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
-      val end = new DateTime(2015, 4, 21, 18, 0, 0, DateTimeZone.UTC)
 
-      val t = TimeSpecification.parse("2015-04-21T12:00:00+00/2015-04-21T18:00:00+00")
-      t.get must equalTo(Seq(start to end))
+    "fail to parse empty spec" in {
+      TimeSpecification.parse("") must beFailedTry
     }
-
-    "parse single time instant?" in {
-      val start = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
-      val t = TimeSpecification.parse("2015-04-21T12:00:00+00")
-      t.get must equalTo(Seq(start to start))
-    }
-
-    "parse time range with duration" in {
-      val start = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
-      val end = new DateTime(2015, 4, 21, 18, 0, 0, DateTimeZone.UTC)
-      val t = TimeSpecification.parse("2015-04-21T12:00:00+00/PT6H")
-      t.get must equalTo(Seq(start to end))
-    }
-
-    // ISO-8601 extensions with repeating intervals which do not exist in the ISO standard
 
     "parse current time" in {
       val before = DateTime.now
@@ -70,48 +52,132 @@ class TimeSpecificationSpec extends Specification {
       new Interval(before, after).contains(now) must beTrue
     }
 
-    "parse repeating consecutive time intervals" in {
-      val dates = Seq(
+    "parse time interval" in {
+      val start = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 21, 18, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T12:00:00+00/2015-04-21T18:00:00+00").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T12:00:00/2015-04-21T18:00:00").get must equalTo(Seq(start to end))
+    }
+
+    "parse time interval with reduced precision" in {
+      val start = new DateTime(2015, 1, 1, 0, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 1, 1, 6, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-01T00:00:00/2015-01T06:00:00").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-01T00:00/2015-01T06:00").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-01T00/2015-01T06").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-01/2015-01T06").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015/2015-01T06").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015/2015T06").get must equalTo(Seq(start to end))
+    }
+
+    "parse time interval with decimal fraction" in {
+      val start = new DateTime(2015, 4, 21, 6, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 21, 12, 0, 30, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T12:00,5").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T12:00.5").get must equalTo(Seq(start to end))
+    }
+
+    "parse time interval with UTC offset" in {
+      val start = new DateTime(2015, 4, 21, 6, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T13:00:00+01").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T11:00:00-01").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T13:45:00+01:45").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T06:00:00/2015-04-21T13:45:00+0145").get must equalTo(Seq(start to end))
+    }
+
+    "parse time interval with 24/0 alternative" in {
+      val start = new DateTime(2015, 4, 21, 0, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 27, 0, 0, 0, DateTimeZone.UTC)
+      val skipReason = "hour=24 currently unsupported (but allowed in ISO 8601)"
+      TimeSpecification.parse("2015-04-21T00:00/2015-04-27T00:00").get must equalTo(Seq(start to end)).orSkip(skipReason)
+      TimeSpecification.parse("2015-04-21T00:00/2015-04-26T24:00").get must equalTo(Seq(start to end)).orSkip(skipReason)
+      TimeSpecification.parse("2015-04-20T24:00/2015-04-27T00:00").get must equalTo(Seq(start to end)).orSkip(skipReason)
+      TimeSpecification.parse("2015-04-20T24:00/2015-04-26T24:00").get must equalTo(Seq(start to end)).orSkip(skipReason)
+    }
+
+    "parse time interval with week date" in {
+      val start = new DateTime(2014, 12, 28, 0, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 1, 2, 0, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2014-W52-7/2015-W01-5").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2014W527/2015W015").get must equalTo(Seq(start to end)).orSkip(
+        "compact week date notation unsupported (but allowed in ISO 8601)")
+    }
+
+    "parse time interval with ordinal date" in {
+      val start = new DateTime(1981, 4, 5, 0, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(1981, 4, 6, 0, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("1981-04-095/1981-04-096").get must equalTo(Seq(start to end)).orSkip(
+        "ordinal dates currently unsupported (but allowed in ISO 8601)")
+    }
+
+    "fail to parse time interval without T delimiter" in {
+      TimeSpecification.parse("2015-04-21 12:00:00/2015-04-21 18:00:00").get must throwA[IllegalArgumentException]
+    }
+
+    "fail to parse negative time interval" in {
+      TimeSpecification.parse("2015-04-21T18:00:00/2015-04-21T12:00:00").get must throwA[IllegalArgumentException]
+    }
+
+    "parse empty time interval" in {
+      val dt = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T12:00:00").get must equalTo(Seq(dt to dt))
+      TimeSpecification.parse("2015-04-21T12:00:00/2015-04-21T12:00:00").get must equalTo(Seq(dt to dt))
+    }
+
+    "parse time interval with concise representation" in {
+      val dt = new DateTime(2015, 2, 3, 4, 5, 6, DateTimeZone.UTC)
+      val skipReason = "concise interval representation currently unsupported (but allowed in ISO 8601)"
+      TimeSpecification.parse("2015-02-03T04:05:06/02-03T04:05:06").get must equalTo(Seq(dt to dt)).orSkip(skipReason)
+      TimeSpecification.parse("2015-02-03T04:05:06/03T04:05:06").get must equalTo(Seq(dt to dt)).orSkip(skipReason)
+      TimeSpecification.parse("2015-02-03T04:05:06/04:05:06").get must equalTo(Seq(dt to dt)).orSkip(skipReason)
+      TimeSpecification.parse("2015-02-03T04:05:06/05:06").get must equalTo(Seq(dt to dt)).orSkip(skipReason)
+      TimeSpecification.parse("2015-02-03T04:05:06/06").get must equalTo(Seq(dt to dt)).orSkip(skipReason)
+    }
+
+    "parse time interval with duration" in {
+      val start = new DateTime(2015, 4, 21, 12, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 21, 18, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T12:00:00/PT6H").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T12:00:00/PT0H").get must equalTo(Seq(start to start))
+      TimeSpecification.parse("PT6H/2015-04-21T18:00:00").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("PT0H/2015-04-21T12:00:00").get must equalTo(Seq(start to start))
+    }
+
+    "parse time interval with duration and UTC offset" in {
+      val start = new DateTime(2015, 4, 21, 10, 0, 0, DateTimeZone.UTC)
+      val end = new DateTime(2015, 4, 21, 16, 0, 0, DateTimeZone.UTC)
+      TimeSpecification.parse("2015-04-21T12:00:00+02/PT6H").get must equalTo(Seq(start to end))
+      TimeSpecification.parse("2015-04-21T08:00:00-02/PT6H").get must equalTo(Seq(start to end))
+    }
+
+    "parse repeating consecutive time intervals (i.e. implicit repetition offset)" in {
+      TimeSpecification.parse("R4/2005-07-01T00:00/2005-07-01T06:00").get must equalTo(Seq(
         new DateTime(2005, 7, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 1, 6, 0, 0, DateTimeZone.UTC),
         new DateTime(2005, 7, 1, 6, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 1, 12, 0, 0, DateTimeZone.UTC),
         new DateTime(2005, 7, 1, 12, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 1, 18, 0, 0, DateTimeZone.UTC),
-        new DateTime(2005, 7, 1, 18, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 2, 0, 0, 0, DateTimeZone.UTC))
-      val t = TimeSpecification.parse("R4/2005-07-01T00:00/2005-07-01T06:00")
-      t.get must equalTo(dates)
+        new DateTime(2005, 7, 1, 18, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 2, 0, 0, 0, DateTimeZone.UTC)))
     }
 
-    "parse repeating time intervals with duration" in {
-      val dates = Seq(
+    // --- BEGIN ISO 8601 extension (explicit repetition offset after the Rn/<interval> form)
+
+    "parse repeating non-consecutive time intervals (i.e. explicit repetition offset)" in {
+      TimeSpecification.parse("R4/2005-07-01T00:00/2005-07-01T06:00/P1D").get must equalTo(Seq(
         new DateTime(2005, 7, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 1, 6, 0, 0, DateTimeZone.UTC),
         new DateTime(2005, 7, 2, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 2, 6, 0, 0, DateTimeZone.UTC),
         new DateTime(2005, 7, 3, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 3, 6, 0, 0, DateTimeZone.UTC),
-        new DateTime(2005, 7, 4, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 4, 6, 0, 0, DateTimeZone.UTC))
-      val t = TimeSpecification.parse("R4/2005-07-01T00:00/2005-07-01T06:00/P1D")
-      t.get must equalTo(dates)
-    }
-
-    //The following query retrieves data for august and september only, for three years (2004 - 2006).
-    // R3/2004-08-01T00:00/2004-10-01T00:00/P1Y
-
-    "parse repeating time ranges with duration" in {
-      val dates = Seq(
+        new DateTime(2005, 7, 4, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 7, 4, 6, 0, 0, DateTimeZone.UTC)))
+      TimeSpecification.parse("R3/2004-08-01T00:00/2004-09-01T00:00/P1Y").get must equalTo(Seq(
         new DateTime(2004, 8, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2004, 9, 1, 0, 0, 0, DateTimeZone.UTC),
         new DateTime(2005, 8, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2005, 9, 1, 0, 0, 0, DateTimeZone.UTC),
-        new DateTime(2006, 8, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2006, 9, 1, 0, 0, 0, DateTimeZone.UTC))
-      val t = TimeSpecification.parse("R3/2004-08-01T00:00/2004-09-01T00:00/P1Y")
-      t.get must equalTo(dates)
+        new DateTime(2006, 8, 1, 0, 0, 0, DateTimeZone.UTC) to new DateTime(2006, 9, 1, 0, 0, 0, DateTimeZone.UTC)))
     }
 
-    "illegal format" in {
+    "fail to parse missing Rn prefix" in {
       TimeSpecification.parse("2008-09-01T12:00/2008-09-01T13:00/P24H") must beFailedTry
     }
 
-    "timezones in spec" in {
-      val start = new DateTime(2015, 4, 21, 10, 0, 0, DateTimeZone.UTC)
-      val end = new DateTime(2015, 4, 21, 16, 0, 0, DateTimeZone.UTC)
-      val t = TimeSpecification.parse("2015-04-21T12:00:00+02/PT6H")
-      t.get must equalTo(Seq(start to end))
-    }
+    // --- END ISO 8601 extension
 
     "find min/max time in range" in {
       val p: String => Try[TimeSpecification.Range] = TimeSpecification.parse
